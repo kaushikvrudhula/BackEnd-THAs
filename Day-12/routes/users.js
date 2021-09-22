cbef31ee17e8a8d47ee4a5891889d4d00f968764
kaushikvrudhula <kvrudhula07@gmail.com>
2021-09-22 19:11:35 +0530

    day-11 and 12 done

--- Day-12/routes/users.js
+++ Day-12/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
+const { Pool } = require('pg');
 var router = express.Router();
 
 /* GET users listing. */
+const pool = new Pool({
+  user:'postgres',
+  host:'postgres',
+  database: 'postgres',
+  password: '123456',
+  passwordport: 5432
+})
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  pool.query('SELECT * FROM "Users"',(err,result) => {
+    if(err){
+      throw err;
+    
+    }
+    res.status(200).json(result)
+  })
 });
 
 module.exports = router;
